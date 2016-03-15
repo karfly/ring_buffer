38c05e51e46adbe730c550fc9b6c9f30eac3e813
karfly <karfly@yandex.ru>
2016-03-15 18:34:38 +0300

    Added ring buffer

---
+++ ring_buffer.h
@@ -0,0 +1,25 @@
+#ifndef RING_BUFFER_RING_BUFFER_H
+#define RING_BUFFER_RING_BUFFER_H
+
+#define RING_BUFFER_SIZE 1024
+
+typedef enum
+        {
+        SUCCESS          = 0,
+        ERROR            = 1,
+        WRONG_ARGUMENTS  = 2,
+        } ret_code_t;
+
+typedef struct ring_buffer ring_buffer_t;
+typedef void   ring_buffer_elem_t;
+
+ret_code_t ring_buffer_construct(ring_buffer_t ** , uint32_t);
+ret_code_t ring_buffer_destruct(ring_buffer_t **);
+
+ret_code_t ring_buffer_put(ring_buffer_t * , const ring_buffer_elem_t *, double *);
+ret_code_t ring_buffer_get(ring_buffer_t * , ring_buffer_elem_t * );
+
+ret_code_t ring_buffer_print(ring_buffer_t *, void (*) (const ring_buffer_elem_t *));
+
+
+#endif //RING_BUFFER_RING_BUFFER_H
